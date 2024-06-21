79691d5e5d67da97bd3ef0f67a3d39a46e1b1cd5
Denis <kripmrx@gmail.com>
2024-06-21 17:12:41 +0200

    set windows size

--- parser.py
+++ parser.py
@@ -28,9 +28,11 @@ class Parser:
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
 
+
         self.cookies = [{"name":"yandexuid","value":"384142161716910258"},{"name":"spravka","value":"dD0xNjg1NjEwOTQ3O2k9MmEwMDoxZmEyOjQxMzE6NTczODpkZjVkOjdlYjQ6OGNmOjg1OGM7RD1EMEYwRTZEQURBRjhEOTZDNDdCRTJFODc2RTUyMDBBQzE2MjY0MkQxOEMwMTNFNUIyM0M3MzE3Q0M5MkFDN0QwQUFEOUM0RDA5NkY4Q0JGRDt1PTE2ODU2MTA5NDc4MDU3OTQwODY7aD00NjVmZmY2NDk3NTA2MTc5Y2ZmMmJlZmE5NGE2NzA0YQ"},{"name":"amcuid","value":"1640817451717147532"},{"name":"is_gdpr","value":"0"},{"name":"is_gdpr_b","value":"COG2HhDp/wE"},{"name":"yuidss","value":"384142161716910258"},{"name":"yp","value":"2034180096.udn.cDpkZW4tMDkta2Fk"},{"name":"L","value":"dVNoAwFacF1LXlNtTFEJe1QEflFqQAlUNh0GQlt8SzMlVg"},{"name":"yandex_login","value":"den-09-kad"},{"name":"ys","value":"udn.cDpkZW4tMDkta2Fk#c_chck.3705171551"},{"name":"bh","value":"EkEiQ2hyb21pdW0iO3Y9IjEyNCIsICJHb29nbGUgQ2hyb21lIjt2PSIxMjQiLCAiTm90LUEuQnJhbmQiO3Y9Ijk5IhoFIng4NiIiDyIxMjQuMC42MzY3Ljc4IioCPzAyAiIiOgciTGludXgiQgciNi44LjkiSgQiNjQiUlsiQ2hyb21pdW0iO3Y9IjEyNC4wLjYzNjcuNzgiLCAiR29vZ2xlIENocm9tZSI7dj0iMTI0LjAuNjM2Ny43OCIsICJOb3QtQS5CcmFuZCI7dj0iOTkuMC4wLjAiWgI/MGCHiNCzBg"},{"name":"font_loaded","value":"YSv1"},{"name":"_yasc","value":"vnqyaO564XFRiW2EIya2islMdcV4Cpd+KOhYipjBKPkstAPv31539c5RxxNMCvW/CGg"},{"name":"eda_web","value":"{%22app%22:{%22analyticsSession%22:{%22id%22:%22lxnega9z-e5n4zdl6a3e-6n9taztbqf-cq4egpft4zv%22%2C%22start%22:1718896260%2C%22update%22:1718909288}%2C%22deliveryTime%22:null%2C%22themeVariantKey%22:%22light%22%2C%22xDeviceId%22:%22lxlwrtcf-bam6qv5dbog-x4dme4v1qsf-3wbmu89kavg%22%2C%22lastObtainedGps%22:{%22lat%22:38.883333%2C%22lon%22:-77%2C%22timestamp%22:1718824098445}%2C%22lat%22:57.147566445521214%2C%22lon%22:65.55251315305341}}"}]
 
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        self.driver.set_window_size(1800, 1080)
 
     def scroll_down(self) -> None:
         """
@@ -56,7 +58,7 @@ class Parser:
 
     def get_page(self) -> None:
         self.driver.get(self.url)
-        
+
         for cookie in self.cookies:
             self.driver.add_cookie({
                 'name': cookie['name'],
